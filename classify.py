91c5124257a150435579bb9f99c97f7718afaa30
Chandler <chandler.baggett@colorado.edu>
2018-02-24 19:03:34 -0700

    Data prepared

--- classify.py
+++ classify.py
@@ -2,3 +2,13 @@ import pandas as pd
 
 df = pd.read_csv("data/wine-reviews/winemag-data-130k-v2.csv")
 
+#print(len(df))
+#print(type(df))
+desc = df['description']
+print(desc)
+
+from sklearn.feature_extraction.text import CountVectorizer
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(desc)
+print(X_train_counts)
+print(X_train_counts.shape)
