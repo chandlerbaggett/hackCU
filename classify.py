a41daed0fb8e0a8f02d631ee78d88e77346ad7ba
Chandler <chandler.baggett@colorado.edu>
2018-02-24 21:55:04 -0700

    Complete NB classification system

--- classify.py
+++ classify.py
@@ -27,18 +27,19 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range=(1,2), stop_words='english')
 features = tfidf.fit_transform(df.description).toarray()
 labels = df.category_id
+#print(type(df.category_id))
 #print(features.shape)
 
-from sklearn.feature_selection import chi2
-import numpy as np
-N = 2
-for variety, category_id in sorted(category_to_id.items()):
-	features_chi2 = chi2(features, labels == category_id)
-	indices = np.argsort(features_chi2[0])
-	feature_names = np.array(tfidf.get_feature_names())[indices]
-	unigrams = [v for v in feature_names if len(v.split(' ')) == 1]
-	bigrams = [v for v in feature_names if len(v.split(' ')) == 2]
-	print("# '{}':".format(variety))
-	print(" . Most correlated unigrams:\n {}".format('\n.'.join(unigrams[-N:])))
-	print(" . Most correlated bigrams:\n. {}".format('\n.'.join(bigrams[-N:])))
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+
+X_train, X_test, y_train, y_test = train_test_split(df['description'], df['variety'], random_state = 0)
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(X_train)
+tfidf_transformer = TfidfTransformer()
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+
+clf = MultinomialNB().fit(X_train_tfidf, y_train.astype(str))
 
